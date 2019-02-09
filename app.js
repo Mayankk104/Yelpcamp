db65e47e5b6492a78bb356eefad7617d1546043f
Mayank kumar <Mayankk104@gmail.com>
2019-02-09 20:49:07 +0530

    minor change for heroku

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.use(loginRoute.loginRoute);
 //  app.listen(3000);
 //});
 
-app.listen(3000, function () {
+app.listen(process.env.PORT||3000, function () {
     console.log("Server is running");
 });
