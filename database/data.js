b4d9f245e859414a4f286e9f284fcf9760242d75
Mayank kumar <Mayankk104@gmail.com>
2019-02-20 19:56:20 +0530

    ,

--- database/data.js
+++ database/data.js
@@ -2,7 +2,7 @@ const mongodb     = require('mongodb'),
       mongoClient = mongodb.MongoClient;
 
 const mongoConnect = callback =>{
-mongoClient.connect('mongodb+srv://Mayankk104:MongoMayank@cluster0-itcku.mongodb.net/Yelpcamp?retryWrites=true')
+mongoClient.connect('mongodb+srv://Mayankk104:MongoMayank@cluster0-itcku.mongodb.net/Yelpcamp?retryWrites=true',{useNewUrlParser:true })
     .then(client =>{
     console.log('Connected')
     callback(client);})
